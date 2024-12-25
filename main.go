6f1269ba1d9d1525e205a29072817c5cba3d7781
Paul Tobin <tobster111@gmail.com>
2024-12-25 13:40:39 +0000

    add route to return custom greeting message

--- main.go
+++ main.go
@@ -11,5 +11,7 @@ func main() {
 	fmt.Println("Hello, World!")
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error { return c.SendString("Hello, Worldasdfas!") })
+
 	log.Fatal(app.Listen(":4000"))
 }
