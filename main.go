8c4bfbf1f02332d828eb816a7916859cdba37788
Paul Tobin <tobster111@gmail.com>
2024-12-25 13:23:39 +0000

    add logging for server startup

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,5 +11,5 @@ func main() {
 	fmt.Println("Hello, World!")
 	app := fiber.New()
 
-	app.Listen(":4000")
+	log.Fatal(app.Listen(":4000"))
 }
