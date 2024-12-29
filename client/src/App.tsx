8b13c6af12517a12a26c1f50c611abb956072fc2
Paul Tobin <tobster111@gmail.com>
2024-12-29 11:47:18 +0000

    feat: add TodoForm component and integrate it into the App layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,15 @@
-import { Stack } from "@chakra-ui/react";
+import { Stack, Container } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   return (
     <>
       <Stack h="100vh">
         <Navbar />
-        {/* <Container>
-          <TodoForm>
-            <TodoList />
-          </TodoForm>
-        </Container> */}
+        <Container>
+          <TodoForm>{/* <TodoList /> */}</TodoForm>
+        </Container>
       </Stack>
     </>
   );
